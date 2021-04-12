e2ddc9301c6bdfba825f3b73fdf9d3e277987a07
naaa187 <knana18@ewhain.net>
2021-04-12 19:02:48 +0900

    edit list

--- src/app/nana_MarketList.tsx
+++ src/app/nana_MarketList.tsx
@@ -1,50 +1,51 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router'
+import { Button } from 'src/components/buttons/Button'
+import HeartIcon from 'src/components/icons/heart.svg'
 import { Box } from 'src/components/layout/Box'
-import { ScreenContentFrame } from 'src/components/layout/ScreenContentFrame'
 import { Font } from 'src/styles/fonts'
 import { mq } from 'src/styles/mediaQueries'
 import { Stylesheet } from 'src/styles/types'
 
 interface ListProps {
-  Id: any
+  id: any
   name: string
-  img: string
-  hashtag: string
-  nft: any
+  mainpic: string
+  categ: string
+  clap: any
 }
 
 export function MarketList() {
   const navigate = useNavigate()
 
   const onClickCreateNew = () => {
-    navigate('/setup/NFTpaint')
+    navigate('/store-detail')
   }
 
   const marketplace = 'Venice High Flea Market'
 
   const markets = [
     {
-      Id: 1,
+      id: 1,
       name: 'Holliday House',
-      img: '../static/nft/bigdog.png',
-      hashtag: '#kitchen',
-      nft: 1,
+      mainpic: '../static/market1.jpg',
+      categ: '#kitchen',
+      clap: 1,
     },
     {
-      Id: 2,
+      id: 2,
       name: 'Home Run Park',
-      img: '../static/nft/puppy.png',
-      hashtag: '#sporting',
-      nft: 0,
+      mainpic: '../static/nft/puppy.png',
+      categ: '#sporting',
+      clap: 0,
     },
     {
-      Id: 3,
+      id: 3,
       name: 'Radio&Camera ground',
-      img: '../static/nft/cat.png',
-      hashtag: '#electronics',
-      nft: 2,
+      mainpic: '../static/nft/cat.png',
+      categ: '#electronics',
+      clap: 2,
     },
   ]
 
@@ -64,42 +65,33 @@ export function MarketList() {
   }, [])
 
   const ListComponent = (list: ListProps) => (
-    <Box>
-      <a href={`/product/${list.Id}`}>
-        <h1 css={style.h1}>{list.name}</h1>
-        <img src={list.img} />
-      </a>
+    <Box css={style.listComponent}>
+      <img src={list.mainpic} css={style.mainpic} />
+      <div className="textBox" style={style.storeProfile}>
+        <h2 css={style.h1}>{list.name}</h2>
+        <Button styles={style.categ}>{list.categ}</Button>
+      </div>
+      <div className="clap" css={{ position: 'absolute', right: '1em', padding: '1.5em 0' }}>
+        <img src={HeartIcon} />
+      </div>
     </Box>
   )
 
   return (
-    <ScreenContentFrame>
-      <Box direction="column" justify="center">
-        <Box direction="column" justify="start" align="start">
-          <h1 css={style.h1}>{marketplace}</h1>
-        </Box>
-        <div>
-          <Box css={style.listContainer}>
-            {markets.map((market) => (
-              <ListComponent key={market.Id} {...market} />
-            ))}
-          </Box>
-        </div>
+    <Box direction="column" justify="center">
+      <Box justify="start" align="start">
+        <h1 css={style.h1}>{marketplace}</h1>
       </Box>
-    </ScreenContentFrame>
+      <Box direction="column" css={style.listContainer}>
+        {markets.map((market) => (
+          <ListComponent key={market.id} {...market} />
+        ))}
+      </Box>
+    </Box>
   )
 }
 
 const style: Stylesheet = {
-  topPadding: {
-    height: '3em',
-    [mq[768]]: {
-      height: '3em',
-    },
-  },
-  logo: {
-    maxWidth: '75%',
-  },
   h1: {
     ...Font.h1,
     ...Font.bold,
@@ -115,11 +107,26 @@ const style: Stylesheet = {
   listContainer: {
     marginTop: '1.5em',
     display: 'block',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
   },
   listComponent: {
-    width: '100vh',
+    width: '100%',
+    padding: '1em',
+    height: '8em',
+    display: 'flex',
+  },
+  mainpic: {
+    margin: 'auto 0',
+    width: '5em',
+    height: '5em',
+    borderRadius: 5,
+  },
+  storeProfile: {
+    margin: '1em',
+  },
+  categ: {
+    height: '1.5em',
+    minWidth: '10em',
+    padding: '0 auto',
+    borderRadius: 50,
   },
 }
