c3620a8839f3abed7f932883c62eb6c891e555c8
naaa187 <knana18@ewhain.net>
2021-04-13 22:26:19 +0900

    edit marketform

--- src/app/nana_MarketForm.tsx
+++ src/app/nana_MarketForm.tsx
@@ -1,32 +1,27 @@
 import { Input } from 'antd'
 import axios from 'axios'
 import { useState } from 'react'
-import { useSelector } from 'react-redux'
-import { RootState } from 'src/app/rootReducer'
+// import Uploading from 'src/app/utils/Uploading'
 import { Box } from 'src/components/layout/Box'
-import { mq } from 'src/styles/mediaQueries'
 import { Stylesheet } from 'src/styles/types'
 
-export default function MarketForm() {
+export default function MarketForm(userId: any) {
   type Istore = {
-  id: string,
-  name: string,
-  categ: string,
-  desc: string,
-  location:string,
-  period: string,
-  hour: string,
-  website: string,
-  mainpic: string,
-  pic1: string,
-  pic2: string,
-  pic3: string,
-  clap: number,
+    id: string
+    name: string
+    categ: string
+    desc: string
+    location: string
+    period: string
+    hour: string
+    website: string
+    mainpic: string
+    pic1: string
+    pic2: string
+    pic3: string
   }
-  const storeId= useSelector((s: RootState) => s.user.userId)
-  console.log(storeId)
   const [store, setStore] = useState<Istore>({
-    id: storeId,
+    id: userId,
     name: '',
     categ: '',
     desc: '',
@@ -36,9 +31,8 @@ export default function MarketForm() {
     website: '',
     mainpic: '',
     pic1: '',
-    pic2: '', 
+    pic2: '',
     pic3: '',
-    clap: 0,
   })
   const onChange = (e: any) => {
     setStore({
@@ -50,58 +44,105 @@ export default function MarketForm() {
 
   const handleSubmit = () => {
     axios
-    .post(`http://ec2-3-34-14-143.ap-northeast-2.compute.amazonaws.com:8000/server/store/`,store)
-    .then((response) => {
-      console.log(response)
-      setStore(response.data)
-      console.log(store)
-    })
-    .catch(function (error) {
-      alert(error)
-      console.log(error)
-    })
+      .get(`http://ec2-3-34-14-143.ap-northeast-2.compute.amazonaws.com:8000/server/store/1`)
+      .then((response) => {
+        console.log(response)
+        setStore(response.data)
+      })
+      .catch(function (error) {
+        alert(error)
+        console.log(error)
+      })
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <Box direction="column" align="center" styles={style.inputContainer}>
-        <h2>Add My Market</h2>
-        <Box direction="row" align="center">
-          {/* <Uploading opt="mainpic" setStore={setStore}/>
-          <Uploading opt="pic1" setStore={setStore}/>
-          <Uploading opt="pic2" setStore={setStore}/>
-          <Uploading opt="pic3" setStore={setStore}/> */}
+        <Box direction="column" align="center">
+          {/* <Uploading opt="mainpic" setStore={setStore} style={{ width: '90%' }} />
+          <Box direction="row" align="center">
+            <Uploading opt="pic1" setStore={setStore} />
+            <Uploading opt="pic2" setStore={setStore} />
+            <Uploading opt="pic3" setStore={setStore} />
+          </Box> */}
         </Box>
-        <Box direction="column" margin="1em 0 0 0">
-          <span css={style.inputLabel}>market name</span>
-          <Input size="large" name="name" type="text" value={store.name} onChange={onChange} />
+        <Box direction="column" styles={style.formContent}>
+          <Box styles={style.info}>
+            <h2>Information</h2>
+          </Box>
+          <span css={style.inputLabel}>Market name</span>
+          <Input
+            size="large"
+            name="name"
+            type="text"
+            placeholder="Please fill out market name"
+            value={store.name}
+            css={style.input}
+            onChange={onChange}
+          />
+          <span css={style.inputLabel}>Category</span>
+          <Input
+            size="large"
+            name="categ"
+            type="text"
+            placeholder="ex. #furniture, #kitchen"
+            value={store.categ}
+            css={style.input}
+            onChange={onChange}
+          />
+          <span css={style.inputLabel}>Intro</span>
+          <Input
+            size="large"
+            name="desc"
+            type="text"
+            placeholder="Please fill out the introduction of your store."
+            value={store.desc}
+            height="5em"
+            css={style.input}
+            onChange={onChange}
+          />
+          <span css={style.inputLabel}>Address</span>
+          <Input
+            size="large"
+            name="location"
+            type="text"
+            value={store.location}
+            css={style.input}
+            onChange={onChange}
+          />
+          <span css={style.inputLabel}>Period of market</span>
+          <Input
+            size="large"
+            name="period"
+            type="text"
+            value={store.period}
+            css={style.input}
+            onChange={onChange}
+          />
+          <span css={style.inputLabel}>Operating hours</span>
+          <Input
+            size="large"
+            name="hour"
+            type="text"
+            value={store.hour}
+            css={style.input}
+            onChange={onChange}
+          />
+          <span css={style.inputLabel}>Website</span>
+          <Input
+            size="large"
+            name="website"
+            type="text"
+            placeholder="Please fill out website"
+            value={store.website}
+            css={style.input}
+            onChange={onChange}
+          />
         </Box>
-        <Box direction="column" margin="1em 0 0 0">
-          <span css={style.inputLabel}>category</span>
-          <Input size="large" name="categ" type="text" value={store.categ} onChange={onChange} />
-        </Box>
-        <Box direction="column" margin="1em 0 0 0">
-          <span css={style.inputLabel}>description</span>
-          <Input size="large" name="desc" type="text" value={store.desc} onChange={onChange} />
-        </Box>
-        <Box direction="column" margin="1em 0 0 0">
-          <span css={style.inputLabel}>loaction</span>
-          <Input size="large" name="location" type="text" value={store.location} onChange={onChange} />
-        </Box>
-        <Box direction="column" margin="1em 0 0 0">
-          <span css={style.inputLabel}>open period</span>
-          <Input size="large" name="period" type="text" value={store.period} onChange={onChange} />
-        </Box>
-        <Box direction="column" margin="2em 0 0 0">
-          <span css={style.inputLabel}>open hour</span>
-          <Input size="large" name="hour" type="text" value={store.hour} onChange={onChange} />
-        </Box>
-        <Box direction="column" margin="1em 0 0 0">
-          <span css={style.inputLabel}>website</span>
-          <Input size="large" name="website" type="text" value={store.website} onChange={onChange} />
-        </Box>
-        <Box direction="column" align='center'>
-          <button style={style.btn} onClick={handleSubmit}>submit</button>
+        <Box direction="row" margin="1em 0 0 0">
+          <button style={style.btn} onClick={handleSubmit}>
+            submit
+          </button>
         </Box>
       </Box>
     </form>
@@ -110,36 +151,40 @@ export default function MarketForm() {
 
 const style: Stylesheet = {
   formContent: {
-    [mq[480]]: {
-      marginLeft: '-1.3em',
-    },
+    margin: '1.2em',
+    display: 'block',
+    width: '90%',
+  },
+  info: {
+    font: 'Poppins-SemiBold',
   },
   inputContainer: {
     marginTop: '1.5em',
-    marginBottom: '5em',
     textAlign: 'right',
   },
   inputLabel: {
     textAlign: 'left',
-    width: '6em',
-    fontSize:'14px',
-    font:'Poppins-Regular',
+    fontSize: '17px',
+    fontWeight: '500',
+    marginBottom: '0.5em',
     paddingRight: '1em',
-    [mq[480]]: {
-      width: '8em',
-    },
+    display: 'block',
+  },
+  input: {
+    backgroundColor: 'rgba(246, 246, 246)',
+    borderRadius: 10,
+    border: '0px',
   },
   btn: {
-    position:'fixed',
-    bottom:'5em',
-    width: '10em',
-    height:'3em',
+    width: '12em',
+    height: '3em',
+    margin: '1.5em',
     background: 'linear-gradient(0.25turn,rgba(247,214,55),rgba(47,207,87))',
     outline: 'none',
     border: 'none',
-    borderRadius: '12px',
+    borderRadius: 50,
     fontWeight: 500,
     color: 'rgba(255,255,255)',
-    font: "Poppins-Medium"
+    font: 'Poppins-Medium',
   },
 }
