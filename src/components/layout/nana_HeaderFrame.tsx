b63596a1783ce3e43d4fb35ff4c891a5b4e93d41
naaa187 <knana18@ewhain.net>
2021-04-13 21:48:48 +0900

    edit header link

--- src/components/layout/nana_HeaderFrame.tsx
+++ src/components/layout/nana_HeaderFrame.tsx
@@ -1,8 +1,8 @@
-import { PropsWithChildren } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Header } from 'src/components/header/nana_Header';
-import { Box } from 'src/components/layout/Box';
-import { Stylesheet } from 'src/styles/types';
+import { PropsWithChildren } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Header } from 'src/components/header/nana_Header'
+import { Box } from 'src/components/layout/Box'
+import { Stylesheet } from 'src/styles/types'
 
 export function HeaderFrame(props: PropsWithChildren<unknown>) {
   const navigate = useNavigate()
@@ -10,8 +10,7 @@ export function HeaderFrame(props: PropsWithChildren<unknown>) {
     <Box direction="column" styles={style.container}>
       <Header />
       <div css={style.content}>{props.children}</div>
-        <button style={style.floating} onClick={()=>navigate('/seller-login')} >
-        </button>
+      <button style={style.floating} onClick={() => navigate('/send')}></button>
     </Box>
   )
 }
@@ -24,15 +23,14 @@ const style: Stylesheet = {
     flex: 1,
     overflow: 'auto',
   },
-  floating:{
-    border:'none',
+  floating: {
+    border: 'none',
     borderRadius: '50%',
     width: '64px',
     height: '64px',
-    position:'fixed',
+    position: 'fixed',
     right: '50px',
     bottom: '50px',
     background: 'url("src/components/icons/wallet.png" )',
   },
-  
 }
